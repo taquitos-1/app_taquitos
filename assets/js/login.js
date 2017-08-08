d8af7ff3f66f2d435335987a6a8dace0de499b58
magentaFire <dafne.magenta@gmail.com>
2017-08-08 12:21:42 -0500

    agrega firebase

---
+++ assets/js/login.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyCFjOvl7UbQdHpi9Ovxexp8QPodVdFXPF8",
+    authDomain: "taquitos-app.firebaseapp.com",
+    databaseURL: "https://taquitos-app.firebaseio.com",
+    projectId: "taquitos-app",
+    storageBucket: "taquitos-app.appspot.com",
+    messagingSenderId: "460263194048"
+};
+
+firebase.initializeApp(config);
