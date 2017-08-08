f9e3e1a66bf44d05d7ef7b30e2001e1410c82cda
ErandyR <rerandy15@gmail.com>
2017-08-08 17:49:16 -0500

    accediendo al valor por local storage

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,47 +1,48 @@
-(function(argument) {
-	
-	var cargarPagina = function () {
-	$('.numeroAleatorio').keydown(validarNumero);
-	var obtenerPerfil = localStorage.getItem('nombre');
-	console.log(obtenerPerfil);
-	var obtenerEmail = localStorage.getItem('correoUsuario');
-	console.log(obtenerEmail);
-	}
+(function (argument) {
 
-let cargarPagina = () => {
-    $("#email").change(validarEmail);
-    $("#telefono").keyup(validarTelefono);
-//    $('#boton').click(direccionar);
-
-    
-}
-
-function validarEmail() { 
-  var expresionRegular =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
-  var email = $("#email").val();
-    if(expresionRegular.test(email)){
-        console.log(email);
-        alert("todo bien");
+    var cargarPagina = function () {
+        var obtenerPerfil = localStorage.getItem('nombre');
+        console.log(obtenerPerfil);
+        $("#nombre").val(obtenerPerfil);
+        var obtenerEmail = localStorage.getItem('correoUsuario');
+        console.log(obtenerEmail);
+        $("#email").val(obtenerEmail);
+        validacionDatos();
     }
-    else {
-        alert("correo invalido");
+
+    var validacionDatos = function() {
+        $("#email").change(validarEmail);
+        $("#telefono").keyup(validarTelefono);
+        //    $('#boton').click(direccionar);
+
+
     }
-}
 
-function validarTelefono(e) {
-    var telefono = $("#telefono").val().trim();
-    var expresionRegular = /^([0-9])*$/;
-    if(telefono.length == 10 && expresionRegular.test(telefono)){
-        console.log(telefono);
-        alert("telefono correcto");
-    } 
-}
+    function validarEmail() {
+        var expresionRegular = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+        var email = $("#email").val();
+        if (expresionRegular.test(email)) {
+            console.log(email);
+            alert("todo bien");
+        } else {
+            alert("correo invalido");
+        }
+    }
 
-function direccionar(e){
-    e.preventDefault();
-    location.href = './restaurantes.html';
-}
+    function validarTelefono(e) {
+        var telefono = $("#telefono").val().trim();
+        var expresionRegular = /^([0-9])*$/;
+        if (telefono.length == 10 && expresionRegular.test(telefono)) {
+            console.log(telefono);
+            alert("telefono correcto");
+        }
+    }
+
+    function direccionar(e) {
+        e.preventDefault();
+        location.href = './restaurantes.html';
+    }
 
 
-	$(document).ready(cargarPagina);
+    $(document).ready(cargarPagina);
 })();
